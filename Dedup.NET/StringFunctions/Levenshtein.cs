fa5dad8ea15051375eecae635c5770d48218412f
agrock86 <agrock86@gmail.com>
2011-03-16 04:53:43 +0000

    implementando clases base de la librería

---
+++ Dedup.NET/StringFunctions/Levenshtein.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DedupeNET.Core;
+
+namespace DedupeNET.StringFunctions
+{
+    public class Levenshtein : DistanceFunction
+    {
+        public Levenshtein(string firstString, string secondString)
+            : base(firstString, secondString)
+        {
+        }
+        
+        public override double Distance()
+        {
+            
+        }
+    }
+}
