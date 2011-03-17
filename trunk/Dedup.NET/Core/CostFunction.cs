3ef559ca417c371e80f9329802b078c86cfd0459
agrock86@gmail.com <agrock86@gmail.com@29342d4c-b102-98d1-e485-585945433af2>
2011-03-17 04:08:45 +0000

    implementando clases base de la librería

---
+++ trunk/Dedup.NET/Core/CostFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DedupeNET.Core
+{
+    public abstract class CostFunction
+    {
+        public abstract double GetCost(char a, char b);
+    }
+}
